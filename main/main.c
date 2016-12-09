c22f3ca7dfb68e6c3475f65b0782e4b443201107
www220@tom.com <www220@tom.com>
2016-12-09 14:52:43 +0800

    显示版本，初始化msh

--- main/main.c
+++ main/main.c
@@ -1,42 +1,37 @@
-#include "freertos/FreeRTOS.h"
-#include "esp_wifi.h"
+/* Hello World Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
 #include "esp_system.h"
-#include "esp_event.h"
-#include "esp_event_loop.h"
 #include "nvs_flash.h"
-#include "driver/gpio.h"
+#include <rtthread.h>
+#include <board.h>
+#include <shell.h>
+#include <msh.h>
 
-esp_err_t event_handler(void *ctx, system_event_t *event)
-{
-    return ESP_OK;
-}
+extern long list_thread(void);
+extern int cmd_time(int argc, char **argv);
 
-void app_main(void)
+void app_main()
 {
     nvs_flash_init();
-    tcpip_adapter_init();
-    ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
-    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
-    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
-    wifi_config_t sta_config = {
-        .sta = {
-            .ssid = "access_point_name",
-            .password = "password",
-            .bssid_set = false
-        }
-    };
-    ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &sta_config) );
-    ESP_ERROR_CHECK( esp_wifi_start() );
-    ESP_ERROR_CHECK( esp_wifi_connect() );
-
-    gpio_set_direction(GPIO_NUM_4, GPIO_MODE_OUTPUT);
-    int level = 0;
-    while (true) {
-        gpio_set_level(GPIO_NUM_4, level);
-        level = !level;
-        vTaskDelay(300 / portTICK_PERIOD_MS);
+    //
+    rt_hw_board_init();
+    rt_show_version();
+    //
+#ifdef RT_USING_FINSH
+    cmd_time(0,0);
+    finsh_system_init();
+    finsh_set_device(FINSH_DEVICE_NAME);
+#endif
+    //
+    while (1)
+    {
+        rt_thread_delay(rt_tick_from_millisecond(1000));
     }
 }
-
