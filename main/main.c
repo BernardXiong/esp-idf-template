1552e271ae4ae2185b5c6a7f870294b18c348f22
www220@tom.com <www220@tom.com>
2016-12-24 19:26:23 +0800

    增加wifi的功能

--- main/main.c
+++ main/main.c
@@ -14,8 +14,57 @@
 #include <shell.h>
 #include <msh.h>
 
+#include "bta_api.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event_loop.h"
+#include "esp_log.h"
+
 extern long list_thread(void);
 extern int cmd_time(int argc, char **argv);
+
+
+static esp_err_t event_handler(void *ctx, system_event_t *event)
+{
+    switch(event->event_id) {
+    case SYSTEM_EVENT_STA_START:
+        esp_wifi_connect();
+        rt_kprintf("STA_START\n");
+        break;
+    case SYSTEM_EVENT_STA_GOT_IP:
+        rt_kprintf("STA_GOT_IP\n");
+        break;
+    case SYSTEM_EVENT_STA_DISCONNECTED:
+        /* This is a workaround as ESP32 WiFi libs don't currently
+           auto-reassociate. */
+        esp_wifi_connect();
+        rt_kprintf("STA_DISCONNECTED\n");
+        break;
+    default:
+        break;
+    }
+    return ESP_OK;
+}
+
+void rt_hw_wifi_init()
+{
+    tcpip_adapter_init();
+    ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
+    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
+    wifi_config_t sta_config = {
+        .sta = {
+            .ssid = "xxxxx",
+            .password = "xxxxx",
+            .bssid_set = false
+        }
+    };
+    ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &sta_config) );
+    ESP_ERROR_CHECK( esp_wifi_start() );
+    ESP_ERROR_CHECK( esp_wifi_connect() );
+}
 
 void app_main()
 {
@@ -29,6 +78,8 @@ void app_main()
     finsh_system_init();
     finsh_set_device(FINSH_DEVICE_NAME);
 #endif
+    //
+    rt_hw_wifi_init();
     //
     while (1)
     {
